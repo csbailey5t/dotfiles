daf1429412f88ed8b98b67c6771cb1f9d04a17e5
Scott Bailey <csb5t@virginia.edu>
2015-03-13 09:48:20 -0400

    initial commit

---
+++ custom.aliases.bash
@@ -0,0 +1,16 @@
+# copy the working directory path
+alias cpwd="pwd|tr -d '\n'|pbcopy"
+
+# better ls
+alias ll='ls -FGLAhp'
+
+# common directories
+alias projects="cd /Users/csb5t/projects"
+alias mamp="cd /Users/csb5t/mamp"
+alias ivanhoe="cd /Users/csb5t/mamp/wordpress/wp-content/themes/ivanhoe"
+
+# brew
+alias bup="brew update && brew upgrade"
+
+# easy moving
+alias ..="cd .."
