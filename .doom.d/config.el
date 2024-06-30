9b80ccd9559d14aa5d7eec6db34bcb0b50edc091
Scott Bailey <csb5t@virginia.edu>
2024-06-30 05:14:46 -0400

    Update doom config; use my own fork of night owl for emacs 30

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -5,46 +5,52 @@
 
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Scott Bailey"
-      user-mail-address "csb5t@virginia.edu")
+      user-mail-address "csbailey5t@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+
 (setq doom-font (font-spec :family "Dank Mono" :size 16)
-      doom-variable-pitch-font (font-spec :family "Libre Baskerville")
-      doom-serif-font (font-spec :family "Libre Baskerville"))
+      doom-variable-pitch-font (font-spec :family "Iosevka Aile")
+      doom-serif-font (font-spec :family "Monaspace Xenon")
+      doom-symbol-font (font-spec :family "Iosevka"))
+
+
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-nord)
-;; (setq doom-theme 'doom-dracula)
-;; (setq doom-theme 'doom-horizon)
-(load-theme 'night-owl t)
-;; (load-theme 'mindre t)
-
+(setq doom-theme 'doom-one)
+                                        ; (load-theme `night-owl t)
 (setq doom-themes-enable-bold t
       doom-themes-enable-italic t)
 
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type `relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-;; (setq org-directory "~/org/")
 (setq org-directory "~/Dropbox/org")
-(setq org-roam-directory "~/Dropbox/org/pages")
-(setq org-roam-dailies-directory "~/Dropbox/org/journals")
-
-;; Set org agenda dir path to try to include all files in the directory
-(setq org-agenda-files `("~/Dropbox/org"))
 
 
 ;; Set states for ORG todos
@@ -58,15 +64,23 @@
   (setq org-enforce-todo-checkbox-dependencies t))
 
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 'relative)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
+;; - `use-package!' for configuring packages
 ;; - `after!' for running code after a package has loaded
 ;; - `add-load-path!' for adding directories to the `load-path', relative to
 ;;   this file. Emacs searches the `load-path' when you load packages with
@@ -74,13 +88,15 @@
 ;; - `map!' for binding new keys
 ;;
 ;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-
-;; Automatic line wrapping
+;;
+;;;; Automatic line wrapping
 (global-visual-line-mode t)
 
 ;; settings from https://github.com/sunnyhasija/Academic-Doom-Emacs-Config
@@ -88,85 +104,71 @@
  window-combination-resize t ;; take window space from all windows
  x-stretch-cursor t ;; stretch cursor to glyph width
  delete-trailing-lines t)
- 
+
 
 (setq undo-limit 80000000 ;; raise undo limit to 80mb
       evil-want-fine-undo t ;; more fine-grained undo in insert mode
       auto-save-default t
       inhibit-compacting-font-caches t ;; cache glyphs
       truncate-string-ellipsis "…")
-      
+
 
 ;; Define where to find projects files
 (setq projectile-project-search-path '("~/projects/"))
 
-;; (use-package  org-super-agenda
-;;   :config
-;;   (setq org-super-agenda-groups `((:name "Today"
-;;                                    :time-grid t
-;;                                    :scheduled today)
-;;                                   (:name "Due today"
-;;                                    :deadline today)
-;;                                   (:name "Important"
-;;                                    :priority "A")
-;;                                   (:name "Overdue"
-;;                                    :deadline past)))
-;;   (org-super-agenda-mode))
+;; Set of options for org-modern
 
-;; (require 'svelte-mode)
+;; (set-face-attribute 'org-modern-symbol nil :family "Iosevka")
 
-(use-package lsp-ui)
+;; Add frame borders and window dividers
+(modify-all-frames-parameters
+ '((right-divider-width . 20)
+   (internal-border-width . 20)))
+(dolist (face '(window-divider
+                window-divider-first-pixel
+                window-divider-last-pixel))
+  (face-spec-reset-face face)
+  (set-face-foreground face (face-attribute 'default :background)))
+(set-face-background 'fringe (face-attribute 'default :background))
 
-;; (use-package lsp-haskell
-;;   :config
-;;   (setq lsp-haskell-process-path-hie "haskell-language-server-wrapper")
-;;   ;; comment/uncomment below to see lsp client/server interactions
-;;   (setq lsp-log-io t)
-;;   )
-
-(org-babel-do-load-languages
- `org-babel-load-languages
- `((emacs-lisp . t)
-   (julia . t)
-   (python . t)
-   (jupyter . t)))
-
-
-;; (require 'ox-ipynb)
-
-;; (add-hook 'vue-mode-hook #'lsp!)
-
-(setq org-auto-align-tags nil
+(setq
+ ;; Edit settings
+ org-auto-align-tags nil
  org-tags-column 0
- org-catch-invisible-edits 'show-and-error
+ org-fold-catch-invisible-edits 'show-and-error
  org-special-ctrl-a/e t
  org-insert-heading-respect-content t
 
-;; Org styling, hide markup etc.
+ ;; Org styling, hide markup etc.
  org-hide-emphasis-markers t
  org-pretty-entities t
- org-ellipsis "…")
+ org-ellipsis "…"
+ org-modern-hide-stars `leading
 
-;; ;; Agenda styling
-;; org-agenda-tags-column 0
-;; org-agenda-block-separator ?─
-;; org-agenda-time-grid
-;; '((daily today require-timed)
-;;    (800 1000 1200 1400 1600 1800 2000)
-;; " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"
-;; org-agenda-current-time-string
-;; "⭠ now ─────────────────────────────────────────────────")
+ ;; Agenda styling
+ org-agenda-tags-column 0
+ org-agenda-block-separator ?─
+ org-agenda-time-grid
+ '((daily today require-timed)
+   (800 1000 1200 1400 1600 1800 2000)
+   " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+ org-agenda-current-time-string
+ "◀── now ─────────────────────────────────────────────────")
 
 (global-org-modern-mode)
 
 (setq mindre-use-more-bold t
- mindre-use-faded-lisp-parens t)
+      mindre-use-faded-lisp-parens t)
 
-;; (setq! lsp-rust-server 'rust-analyzer)
 
-(setq citar-bibliography '("~/Dropbox/bibliography.bib"))
-(setq citar-symbols
-      `((file ,(all-the-icons-faicon "file-o" :face 'all-the-icons-green :v-adjust -0.1) . " ")
-        (note ,(all-the-icons-material "speaker_notes" :face 'all-the-icons-blue :v-adjust -0.3) . " ")
-        (link ,(all-the-icons-octicon "link" :face 'all-the-icons-orange :v-adjust 0.01) . " ")))
-(setq citar-symbol-separator "  ")
+(use-package markdown-mode
+  :hook (markdown-mode . lsp)
+  :config
+  (require 'lsp-marksman))
+
+;; config for jinx spell checker
+(use-package! jinx
+  :hook (emacs-startup . global-jinx-mode)
+  :config
+  (map! :map evil-normal-state-map
+        "z g" #'jinx-correct))
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -1,78 +1,59 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; To install a package with Doom you must declare them here, run 'doom sync' on
-;; the command line, then restart Emacs for the changes to take effect.
-;; Alternatively, use M-x doom/reload.
-
-
-;; Doom's packages are pinned to a specific commit and updated from release to
-;; release. The `unpin!' macro allows you to unpin single packages...
-;(unpin! pinned-package)
-;; ...or multiple packages
-;(unpin! pinned-package another-pinned-package)
-;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
-;(unpin! t)
+;; To install a package with Doom you must declare them here and run 'doom sync'
+;; on the command line, then restart Emacs for the changes to take effect -- or
+;; use 'M-x doom/reload'.
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;(package! some-package)
+;; (package! some-package)
 
-;; To install a package directly from a particular repo, you'll need to specify
-;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
-;; https://github.com/raxod502/straight.el#the-recipe-format
-;(package! another-package
-;  :recipe (:host github :repo "username/repo"))
+;; To install a package directly from a remote git repo, you must specify a
+;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
+;; https://github.com/radian-software/straight.el#the-recipe-format
+;; (package! another-package
+;;   :recipe (:host github :repo "username/repo"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
-;(package! this-package
-;  :recipe (:host github :repo "username/repo"
-;           :files ("some-file.el" "src/lisp/*.el")))
+;; (package! this-package
+;;   :recipe (:host github :repo "username/repo"
+;;            :files ("some-file.el" "src/lisp/*.el")))
 
-;; If you'd like to disable a package included with Doom, for whatever reason,
-;; you can do so here with the `:disable' property:
-;(package! builtin-package :disable t)
+;; If you'd like to disable a package included with Doom, you can do so here
+;; with the `:disable' property:
+;; (package! builtin-package :disable t)
 
 ;; You can override the recipe of a built in package without having to specify
 ;; all the properties for `:recipe'. These will inherit the rest of its recipe
 ;; from Doom or MELPA/ELPA/Emacsmirror:
-;(package! builtin-package :recipe (:nonrecursive t))
-;(package! builtin-package-2 :recipe (:repo "myfork/package"))
+;; (package! builtin-package :recipe (:nonrecursive t))
+;; (package! builtin-package-2 :recipe (:repo "myfork/package"))
 
 ;; Specify a `:branch' to install a package from a particular branch or tag.
 ;; This is required for some packages whose default branch isn't 'master' (which
-;; our package manager can't deal with; see raxod502/straight.el#279)
-;(package! builtin-package :recipe (:branch "develop"))
+;; our package manager can't deal with; see radian-software/straight.el#279)
+;; (package! builtin-package :recipe (:branch "develop"))
 
-;;install org-super-agenda
-;; (package! org-super-agenda)
+;; Use `:pin' to specify a particular commit to install.
+;; (package! builtin-package :pin "1a2b3c4d5e")
 
-;;install reason mode
-;; (package! reason-mode)
 
-;;install svelte major mode
-;; (package! svelte-mode)
+;; Doom's packages are pinned to a specific commit and updated from release to
+;; release. The `unpin!' macro allows you to unpin single packages...
+;; (unpin! pinned-package)
+;; ...or multiple packages
+;; (unpin! pinned-package another-pinned-package)
+;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
+;; (unpin! t)
 
-;; install lsp-ui
-(package! lsp-ui)
 
-;; install ox-ipynb to export org files to ipynb
-;; (package! ox-ipynb :recipe
-;;   (:host github :repo "jkitchin/ox-ipynb"))
 
-;; install night owl theme
-(package! night-owl-theme)
-
-;; install julia lsp
-;; (package! eglot-jl)
-
-;; Add ripgrep for use with projectile
-(package! ripgrep)
-
-;; add vue mode
-;; (package! vue-mode)
+;; install night owl theme from my own fork
+(package! night-owl-theme :recipe
+  (:host github :repo "csbailey5t/night-owl-emacs"))
 
 ;; Add Rougier's nano-theme from nano-emacs
 (package! nano-theme :recipe
@@ -82,17 +63,9 @@
 
 (package! mindre-theme)
 
-;; (package! lsp-grammarly :recipe
-;;   (:host github :repo "emacs-grammarly/lsp-grammarly"))
-
 (package! prism)
 
-(package! imenu-list)
-(package! org-imenu :recipe
-  (:host github :repo "rougier/org-imenu"))
+(package! jinx)
 
-(package! pdf-drop-mode :recipe
-  (:host github :repo "rougier/pdf-drop-mode"))
-
-(package! org-bib-mode :recipe
-  (:host github :repo "rougier/org-bib-mode" :files ("org-bib.el")))
+(use-package gleam-ts-mode
+  :load-path "/Users/scott/projects/gleam-mode")
